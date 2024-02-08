22b88623b50563314b6632d28d72960f41686f9e
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-02-08 17:52:31 -0300

    initial commit

---
+++ src/main/java/com/gabriel/sendgift/SendgiftApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.sendgift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendgiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendgiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/sendgift/SendgiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.sendgift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendgiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
