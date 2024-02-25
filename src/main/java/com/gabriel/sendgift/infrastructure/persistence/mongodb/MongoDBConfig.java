af5dc11dc291c519bd6df3617d096a597e7fe051
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-02-25 19:53:55 -0300

    feat: mongodb configuration

--- src/main/java/com/gabriel/sendgift/infrastructure/persistence/mongodb/MongoDBConfig.java
+++ src/main/java/com/gabriel/sendgift/infrastructure/persistence/mongodb/MongoDBConfig.java
@@ -1,2 +1,21 @@
-package com.gabriel.sendgift.infrastructure.persistence.mongodb;public class MongoDBConfig {
+package com.gabriel.sendgift.infrastructure.persistence.mongodb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration //Load before initialization
+public class MongoDBConfig {
+
+    @Bean
+    public MongoDatabaseFactory mongoConfigure(){
+        //Returning the instance
+        return new SimpleMongoClientDatabaseFactory("mongodb://root:example@localhost:27017/sendgift");
+    }
+
+    public MongoTemplate mongoTemplate(){
+        return new MongoTemplate(mongoConfigure());
+    }
 }
