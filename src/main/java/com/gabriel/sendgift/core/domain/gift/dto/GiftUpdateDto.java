166342e87b3841fb80b17498ad07521a4229b945
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2024-03-04 17:54:54 -0300

    fix: add constructor

--- src/main/java/com/gabriel/sendgift/core/domain/gift/dto/GiftUpdateDto.java
+++ src/main/java/com/gabriel/sendgift/core/domain/gift/dto/GiftUpdateDto.java
@@ -1,9 +1,11 @@
 package com.gabriel.sendgift.core.domain.gift.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.bson.types.ObjectId;
 
 @Data
+@AllArgsConstructor
 public class GiftUpdateDto {
     private String name;
     private String description;
--- src/main/java/com/gabriel/sendgift/core/domain/user/dto/UserUpdateDto.java
+++ src/main/java/com/gabriel/sendgift/core/domain/user/dto/UserUpdateDto.java
@@ -1,8 +1,10 @@
 package com.gabriel.sendgift.core.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserUpdateDto {
     private String name;
     private String cep;
